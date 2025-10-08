8f4db759f7391404d354113d169330c84ecbf05d
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-10-08 14:14:27 +0530

    refector: naming fix

--- browsers-setup.sh
+++ browsers-setup.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+set -e
 
 # Colors
 BLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-echo -e "${BLUE}🔹feat: ensuring snap is installed in system...${NC}"
+echo -e "${BLUE}🔹Ensuring snap is installed in system...${NC}"
 sudo apt install snapd -y
-echo -e "${GREEN}chore: snap is Installed successfully ${NC}"
+echo -e "${GREEN}Snap is Installed successfully ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Brave-Browser...${NC}"
+echo -e "${BLUE}🔹Installing Brave-Browser...${NC}"
 sudo snap install brave
-echo -e "${GREEN}chore: brave-browser installation completed ${NC}"
+echo -e "${GREEN}Brave-browser installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Opera-Browser...${NC}"
+echo -e "${BLUE}🔹Installing Opera-Browser...${NC}"
 sudo snap install opera
-echo -e "${GREEN}chore: opera-browser installation completed ${NC}"
+echo -e "${GREEN}Opera-browser installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Chromium-Browser...${NC}"
+echo -e "${BLUE}🔹Installing Chromium-Browser...${NC}"
 sudo snap install chromium
-echo -e "${GREEN}chore: chromium installation completed ${NC}"
+echo -e "${GREEN}Chromium installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Firefox-Browser...${NC}"
+echo -e "${BLUE}🔹Installing Firefox-Browser...${NC}"
 sudo snap install firefox
-echo -e "${GREEN}chore: firefox installation completed ${NC}"
+echo -e "${GREEN}Firefox installation completed ${NC}"
 
-echo -e "${GREEN}chore: Browsers installed successfully! ${NC}"
+echo -e "${GREEN}All Browsers installed successfully! ${NC}"
 
