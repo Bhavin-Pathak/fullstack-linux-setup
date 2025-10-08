d62ff4f75807537c65ddc014eaca37d8719409b5
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-10-08 14:28:43 +0530

    doc: fix namings

--- flutter-dart-setup.sh
+++ flutter-dart-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Colors
 BLUE='\033[1;34m'
@@ -6,21 +7,22 @@ GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e "${BLUE}🔹feat: ensuring snap is installed in system...${NC}"
+echo -e "${BLUE}🔹Ensuring snap is installed in system...${NC}"
 sudo apt install snapd -y
-echo -e "${GREEN}chore: snap is Installed successfully ${NC}"
+echo -e "${GREEN}Snap is Installed successfully ${NC}"
 
-echo -e "${BLUE}🔹feat: installing flutter-SDK...${NC}"
+echo -e "${BLUE}🔹Installing flutter-SDK...${NC}"
 sudo snap install flutter --classic
-echo -e "${GREEN}chore: flutter-SDK installation completed ${NC}"
+echo -e "${GREEN}Flutter-SDK installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: adding Flutter-Dart to PATH...${NC}"
+echo -e "${BLUE}🔹Adding Flutter-Dart to PATH...${NC}"
 export PATH="$PATH:/snap/bin:/snap/flutter/common/flutter/bin"
 export PATH="$PATH:/snap/flutter/common/flutter/bin/cache/dart-sdk/bin"
+
 echo 'export PATH=$PATH:/snap/bin:/snap/flutter/common/flutter/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:/snap/flutter/common/flutter/bin/cache/dart-sdk/bin' >> ~/.bashrc
-echo -e "${GREEN}chore: adding flutter and dart to PATH Success ${NC}"
+echo -e "${GREEN}Adding flutter and dart to PATH Success ${NC}"
 
-echo -e "${GREEN}chore: flutter and dart setup completed successfully! ${NC}"
-echo -e "${RED}fix: please restart your terminal or run 'source ~/.bashrc' to apply PATH changes.${NC}"
+echo -e "${GREEN}Flutter and dart setup completed successfully! ${NC}"
+echo -e "${RED}Please restart your terminal or run 'source ~/.bashrc' to apply PATH changes.${NC}"
 
