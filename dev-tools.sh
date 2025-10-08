5f6c624d9194b47d70f5625c3c4bd22476d11eb5
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-10-08 14:20:38 +0530

    chore: adding dbeaver on tools

--- dev-tools.sh
+++ dev-tools.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
+set -e
 
 # Colors
 BLUE='\033[1;34m'
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-echo -e "${BLUE}🔹feat: ensuring snap is installed in system...${NC}"
+echo -e "${BLUE}🔹Ensuring snap is installed in system...${NC}"
 sudo apt install snapd -y
-echo -e "${GREEN}chore: snap is Installed successfully ${NC}"
+echo -e "${GREEN}Snap is Installed successfully ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Android Studio...${NC}"
+echo -e "${BLUE}🔹Installing Android Studio...${NC}"
 sudo snap install android-studio --classic
-echo -e "${GREEN}chore: android-studio installation completed ${NC}"
+echo -e "${GREEN}Android-studio installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Visual Studio Code...${NC}"
+echo -e "${BLUE}🔹Installing Visual Studio Code...${NC}"
 sudo snap install code --classic
-echo -e "${GREEN}chore: visual-studio-code installation completed ${NC}"
+echo -e "${GREEN}Visual-studio-code installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Sublime-Text...${NC}"
+echo -e "${BLUE}🔹Installing Sublime-Text...${NC}"
 sudo snap install sublime-text --classic
-echo -e "${GREEN}chore: sublime-text installation completed ${NC}"
+echo -e "${GREEN}Sublime-text installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Notepad++...${NC}"
+echo -e "${BLUE}🔹Installing Notepad++...${NC}"
 sudo snap install notepad-plus-plus
-echo -e "${GREEN}chore: Notepad++ installation completed ${NC}"
+echo -e "${GREEN}Notepad++ installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Postman For API Testing...${NC}"
+echo -e "${BLUE}🔹Installing Postman For API Testing...${NC}"
 sudo snap install postman
-echo -e "${GREEN}chore: postman installation completed ${NC}"
+echo -e "${GREEN}Postman installation completed ${NC}"
 
-echo -e "${BLUE}🔹feat: installing Beekeeper Studio For Database Viewer...${NC}"
+echo -e "${BLUE}🔹Installing Beekeeper Studio For Database Viewer...${NC}"
 sudo snap install beekeeper-studio
-echo -e "${GREEN}chore: beekeeper-studio installation completed ${NC}"
+echo -e "${GREEN}Beekeeper-studio installation completed ${NC}"
 
-echo -e "${GREEN}chore: Development tools installed successfully! ${NC}"
+echo -e "${BLUE}🔹Installing DBeaver-ce For Database Viewer...${NC}"
+sudo snap install dbeaver-ce
+echo -e "${GREEN}DBeaver-ce installation completed ${NC}"
+
+echo -e "${GREEN}Development tools and Database viewer installed successfully! ${NC}"
 
