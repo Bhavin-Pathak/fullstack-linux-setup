0abff7d475b9fd74ff81254ddf695df837fed8ce
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-10-09 11:35:18 +0530

    refector: interpulate with version-info

--- postgresql-setup.sh
+++ postgresql-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Colors
 BLUE='\033[1;34m'
@@ -6,32 +7,38 @@ GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-echo -e "${BLUE}🔹feat: installing prerequisites (curl, ca-certificates)...${NC}"
+echo -e "${BLUE}🔹Installing prerequisites (curl, ca-certificates)...${NC}"
 sudo apt install -y curl ca-certificates
-echo -e "${GREEN}chore: dependency installed completed ${NC}"
+echo -e "${GREEN}Dependency installed completed ${NC}"
 
-echo -e "${BLUE}🔹feat: creating keyring-directory...${NC}"
+echo -e "${BLUE}🔹Creating keyring-directory...${NC}"
 sudo install -d /usr/share/postgresql-common/pgdg
-echo -e "${GREEN}chore: directory created successfully ${NC}"
+echo -e "${GREEN}Directory created successfully ${NC}"
 
-echo -e "${BLUE}🔹feat: downloading PostgreSQL GPG-key...${NC}"
+echo -e "${BLUE}🔹Downloading PostgreSQL GPG-key...${NC}"
 sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
-echo -e "${GREEN}chore: GPG-KEY downloaded sucessfully ${NC}"
+echo -e "${GREEN}GPG-KEY downloaded sucessfully ${NC}"
 
-echo -e "${BLUE}🔹feat: adding PostgreSQL repository...${NC}"
+echo -e "${BLUE}🔹Adding PostgreSQL repository...${NC}"
 . /etc/os-release
 sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-echo -e "${GREEN}chore: repository added successfully ${NC}"
+echo -e "${GREEN}Repository added successfully ${NC}"
 
-echo -e "${BLUE}🔹feat: updating package list...${NC}"
+echo -e "${BLUE}🔹Updating package list...${NC}"
 sudo apt update
-echo -e "${GREEN}chore: packages updated ${NC}"
+echo -e "${GREEN}Packages updated ${NC}"
 
-echo -e "${BLUE}🔹feat: installing PostgreSQL...${NC}"
+echo -e "${BLUE}🔹Installing PostgreSQL...${NC}"
 sudo apt -y install postgresql
-echo -e "${GREEN}chore: postgresql installed successfully ${NC}"
+echo -e "${GREEN}Postgresql installed successfully ${NC}"
+
+echo -e "${BLUE}🔹Starting PostgreSQL...${NC}"
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo systemctl status postgresql
+echo -e "${GREEN}PostgreSQL service started and enabled ${NC}"
 
 echo -e "${GREEN}PostgreSQL installation completed successfully!${NC}"
-echo -e "${RED}fix: To check status, run: sudo systemctl status postgresql${NC}"
-echo -e "${RED}fix: To start service manually: sudo systemctl start postgresql${NC}"
 
+echo -e "${BLUE}Verifying installed versions...${NC}"
+echo -e "${GREEN}PostgreSQL Version is: $(psql --version)${NC}"
